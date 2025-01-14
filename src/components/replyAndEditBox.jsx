677a6d7a471c96804bc6f4302b93d3c2f87a7921
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-14 23:52:09 +0700

    update component replyAndEditbox

--- src/components/replyAndEditBox.jsx
+++ src/components/replyAndEditBox.jsx
@@ -2,25 +2,50 @@ import React from "react";
 import { Button } from "./ui/button";
 import { X } from "lucide-react";
 
-export const ReplyAndEditBox = ({ replyToMessage, onCancel, type }) => {
+export const ReplyAndEditBox = ({
+  replyToMessage,
+  onCancel,
+  type,
+  replayTo = false,
+  isSender,
+}) => {
   return (
-    <div className="border-l-4 flex justify-between border-blue-500 bg-gray-100 mx-4 p-2 rounded">
+    <div
+      className={`border-l-4 flex  justify-between ${
+        replayTo
+          ? `border-[#D5D7DA]  ${
+              !isSender ? "bg-[#F5F5F5]" : "bg-[#528BFF]"
+            } mb-2`
+          : "border-[#2970FF]"
+      } bg-gray-100 p-2 rounded`}
+    >
       <div className="flex flex-col">
-        <div className="text-sm font-bold text-blue-600">
+        <div
+          className={`text-sm ${
+            replayTo ? `${!isSender ? "" : "text-white"}` : "font-bold"
+          }`}
+        >
           {type === "edit" ? "You" : replyToMessage.user}
         </div>
-        <div className="text-xs text-gray-700 truncate max-w-[200px]">
+        <div
+          className={`text-xs ${
+            replayTo ? `${!isSender ? "" : "text-white"}` : ""
+          }
+           truncate w-[160px]`}
+        >
           {replyToMessage.content}
         </div>
       </div>
-      <Button
-        onClick={onCancel}
-        size="icon"
-        variant="bgWhite"
-        className="text-xs"
-      >
-        <X size={16} />
-      </Button>
+      {(type === "edit" || type === "reply") && replayTo === false && (
+        <Button
+          onClick={onCancel}
+          size="icon"
+          variant="bgWhite"
+          className="text-xs"
+        >
+          <X size={16} />
+        </Button>
+      )}
     </div>
   );
 };
